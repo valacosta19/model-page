143b095eee230d4aca6321a83bb095057559943d
Valentina <valen.acosta19@hotmail.com>
2022-03-01 22:14:48 -0300

    New favicon

--- src/components/WhatsappButton/WhatsappButton.js
+++ src/components/WhatsappButton/WhatsappButton.js
@@ -7,7 +7,7 @@ export const WhatsappButton = () => {
   const theme = useTheme();
 
   return (
-    <Link href="https://wa.link/p2tk6d" target="_blank" className='fixed bottom-4 right-4 bg-green rounded-[50%] p-3 transition-all ease-in-out hover:bg-lightblue'>
+    <Link href="https://wa.link/p2tk6d" target="_blank" className='fixed bottom-4 right-4 bg-green rounded-[50%] p-2 transition-all ease-in-out hover:bg-lightblue'>
       <WhatsAppIcon style={{color: theme.palette.common.white, fontSize: 50}} />
     </Link>
   )
